f484a2a85a58328a7d29322209720fc648a2dcd0
Freddy <freedydan@gmail.com>
2020-04-25 00:04:42 +0800

    addpathprefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/flaurensia_profile/",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -27,7 +28,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
